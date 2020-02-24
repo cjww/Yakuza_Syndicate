3bd58785fb62a7d6749c533bf58d75bdc157faa7
Julius Wendt <julius.wendt@live.se>
2020-02-24 14:06:04 +0100

    shader now hanldles text aswell

--- Yakuza_Syndicate/res/fragmentShader.glsl
+++ Yakuza_Syndicate/res/fragmentShader.glsl
@@ -5,12 +5,15 @@ uniform vec4 teamColor;
 
 void main() {
     vec2 texCoord = gl_TexCoord[0].xy;
-    //texCoord.x = 1.0 - texCoord.x;
-    vec4 texPixel = texture2D(texture, texCoord); 
-    if(texPixel == vec4(1, 0, 1, 1)){
+    vec4 texPixel = texture2D(texture, texCoord);
+
+    vec4 switchToColor = vec4(1, 0, 1, 1); //Magenta
+    vec4 switchToShade = vec4(1, 1, 0, 1); //Yellow
+
+    if(texPixel == switchToColor || (gl_Color == switchToColor && texPixel.a > 0.5)) {
         gl_FragColor = teamColor;
     }
-    else if(texPixel == vec4(1, 1, 0, 1)){
+    else if(texPixel == switchToShade){
         gl_FragColor = teamColor * vec4(0.8, 0.8, 0.8, 1);
     }
     else{
